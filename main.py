6aa93c6c771a3501106448cf08f9f42a6567e1ea
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-10-08 16:47:34 +0900

    update

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 
 # 行の表示数の上限を撤廃
-pd.set_option('display.max_rows', None)
+pd.set_option("display.max_rows", None)
 # 列の表示数の上限を撤廃
-pd.set_option('display.max_columns', None)
+pd.set_option("display.max_columns", None)
 
 
 # urlの取得
@@ -23,12 +23,8 @@ def url_setter(target: list[str]) -> list[str]:
 # データ整形
 def data_formatter(df: pd.DataFrame) -> pd.DataFrame:
     df_copy = df.copy()
-    df_copy["騎手"] = df_copy["騎手"].replace(re.compile(r"\n"), '', regex=True)
-    df_copy.drop(
-        columns=["映像", "馬場指数", "ﾀｲﾑ指数", "厩舎ｺﾒﾝﾄ", "備考"],
-        axis=1,
-        inplace=True
-    )
+    df_copy["騎手"] = df_copy["騎手"].replace(re.compile(r"\n"), "", regex=True)
+    df_copy.drop(columns=["映像", "馬場指数", "ﾀｲﾑ指数", "厩舎ｺﾒﾝﾄ", "備考"], axis=1, inplace=True)
     return df_copy
 
 
@@ -46,22 +42,17 @@ def generate_pandas_dataframe(url: str) -> pd.DataFrame:
     return data_formatter(df)
 
 
-# TODO concat再帰的処理
-def concat_df(data_l: list[pd.DataFrame]) -> pd.DataFrame:
-    c_df = pd.concat([data_l[0], data_l[1]])
-    if n < 0:
-        return c_df
-
-
-    return concat_df(c_df, n)
-
-
+# url取得
 url_list = url_setter(data.get_target_pattern())
 
-data_list = []
+# df変換
+df_list = []
 for url in url_list:
     data = generate_pandas_dataframe(url)
-    data_list.append(data)
+    df_list.append(data)
 
+# df併合
+df_horse_data = pd.concat(objs=df_list)
 
-# TODO pd結合 -> concat
+# csv変換
+df_horse_data.to_csv("output.csv", sep=",")
